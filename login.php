0c7a687a1f99dc8db363f8bc95b957de810a38f6
Wendy-Lagho <wendy.lagho@strathmore.edu>
2024-03-12 11:36:13 +0300

    Update login.php

--- login.php
+++ login.php
@@ -5,9 +5,12 @@
     <title>Login</title>
   </head>
   <body>
-    <form action="login.php" method="post">
-      <input type="text" name="username" placeholder="Username">
-      <input type="password" name="password" placeholder="Password">
+    <h2>Login</h2>
+    <form action="login_process.php" method="post">
+      <label for="email">Email: </label><br>
+      <input type="email" id="email" name="email" required><br>
+      <label for="password">Password: </label><br>
+      <input type="password" id="password" name="password" required><br>
       <button type="submit">Login</button>
     </form>
   </body>
