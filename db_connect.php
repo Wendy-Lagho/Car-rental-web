88e24bec8065bca8042ef8c1e2ade097ab64e7fd
Wendy-Lagho <wendy.lagho@strathmore.edu>
2024-03-13 14:05:20 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,16 @@
-<?
+<?php
 $host = "localhost";
 $dbname = "booking";
 $username = "root";
-$password = " ";
+$password = "";
 
-//establish a database connection
+// establish a database connection
 $conn = new mysqli($host, $username, $password, $dbname);
 
-//check if the connection was successful
-if($conn->connect_error){
-  die("Connection failed: " . $conn->connect_error);
+// check if the connection was successful
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+echo "Connected successfully!";
 ?>
