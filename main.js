1746484b7a8f3265d8d149cbf201df48753f99d0
Wendy-Lagho <wendy.lagho@strathmore.edu>
2024-03-19 22:07:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 //interactivity for the app
 //importing the modules
 
-import { getWeatherData } from './weatherData';
-import { updateUI } from './updateUI';
-import { getCity } from './getCity';
+
