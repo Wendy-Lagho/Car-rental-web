527c23b519205612cb24ea34d590254fc8f363a0
Wendy-Lagho <wendy.lagho@strathmore.edu>
2024-06-03 21:23:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+// Import necessary modules or libraries
+// For example:
+// const express = require('express');
+// const bodyParser = require('body-parser');
 
+// Create an instance of the express application
+// const app = express();
+
+// Set up middleware
+// For example:
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// Define routes
+// For example:
+// app.get('/', (req, res) => {
+//   res.send('Hello, world!');
+// });
+
+// Start the server
+// For example:
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
